4ba85f55478f4c6ec4b8bb711be9ecff6724c030
Drew Chase <dcmanproductions@gmail.com>
2023-08-16 18:50:28 -0400

    Initial Commit

--- fabric/src/main/java/chase/minecraft/architectury/modtemplate/fabric/ModTemplateFabric.java
+++ fabric/src/main/java/chase/minecraft/architectury/modtemplate/fabric/ModTemplateFabric.java
@@ -3,9 +3,11 @@ package chase.minecraft.architectury.modtemplate.fabric;
 import chase.minecraft.architectury.modtemplate.ModTemplate;
 import net.fabricmc.api.ModInitializer;
 
-public class ModTemplateFabric implements ModInitializer {
-    @Override
-    public void onInitialize() {
-        ModTemplate.init();
-    }
+public class ModTemplateFabric implements ModInitializer
+{
+	@Override
+	public void onInitialize()
+	{
+		ModTemplate.init();
+	}
 }
